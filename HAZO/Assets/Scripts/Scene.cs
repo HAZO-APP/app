fd355dcf23317dea542e88b98ac6521770a98082
Alina Danilova <adani082@uottawa.ca>
2021-03-07 21:29:00 -0500

    a
    a

---
+++ HAZO/Assets/Scripts/Scene.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+
+    public void Map()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+
+  
+    public void Exit()
+    {
+
+        EditorApplication.isPlaying = false;
+        Application.Quit();
+        //Debug.Break();
+    }
+    public void Back()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+    public void Recycle()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+    public void Account()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Search()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
