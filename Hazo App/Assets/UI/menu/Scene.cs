cc62e2020d3dcd85ba33fd039f240cfbb30b9f35
HasinZaman <34819460+HasinZaman@users.noreply.github.com>
2021-03-09 01:48:28 -0500

    Added Menus and Place holder
    Features
     - Added place holder Images
     - Connected pages
     - Added header
     - Updated Scene script

---
+++ Hazo App/Assets/UI/menu/Scene.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+    public void account()
+    {
+        SceneManager.LoadScene("Account");
+    }
+    public void map()
+    {
+        SceneManager.LoadScene("Map");
+    }
+    public void ar()
+    {
+        SceneManager.LoadScene("AR");
+    }
+
+
+    public void Exit()
+    {
+
+        EditorApplication.isPlaying = false;
+        Application.Quit();
+        //Debug.Break();
+    }
+    public void Back()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+    public void Recycle()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+    public void Search()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
