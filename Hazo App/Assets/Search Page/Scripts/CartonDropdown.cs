bed0e8d7098fda50724be46920400abd86925b9d
Alina Danilova <adani082@uottawa.ca>
2021-03-22 20:14:04 -0400

    Added Scripts

---
+++ Hazo App/Assets/Search Page/Scripts/CartonDropdown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CartonDropdown : MonoBehaviour
+{
+    public TextMeshProUGUI output;
+
+    public void HandleInputData(int val)
+    {
+        if (val == 0)
+        {
+            output.text = "Please Select";
+        }
+        if (val == 1)
+        {
+            output.text = "Recycable, Blue Bin!";
+        }
+        if (val == 2)
+        {
+            output.text = "Recycable, Blue Bin!";
+        }
+        if (val == 3)
+        {
+            output.text = "Recycable, Blue Bin!";
+        }
+        
+    }
+}
