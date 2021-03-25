838214aa9def349f6bbc6172e8eb204c8d9861bd
Alina Danilova <adani082@uottawa.ca>
2021-03-25 14:14:27 -0400

    fixed some of the issues with the dropdown

---
+++ Dropdown/Assets/dropdownexample.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dropdownexample : MonoBehaviour
+{
+    List<string> items = new List<string>() { "Coffee Cup", "Glass Bottle", "Pizza Box", "Water Bottle" };
+
+    public Dropdown dropdown;
+    public Text selectedName;
+
+    public void Dropdown_IndexChanged(int index)
+    {
+        items[0]= "recycable";
+    }
+
+    void Start()
+    {
+        PopulateList();
+    }
+
+
+    void PopulateList()
+   {
+       
+        dropdown.AddOptions(items);
+
+    }
+        
+
+}
---
+++ Hazo App/Assets/Scripts/Scene.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+
+    public void Map()
+    {
+
+        SceneManager.LoadScene(1);
+    }
+
+  
+    public void Exit()
+    {
+
+        EditorApplication.isPlaying = false;
+        Application.Quit();
+        //Debug.Break();
+    }
+    public void Back()
+    {
+
+        SceneManager.LoadScene(0);
+
+    }
+    public void Recycle()
+    {
+
+        SceneManager.LoadScene(2);
+    }
+
+
+}
